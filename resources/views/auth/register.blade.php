3cd915a5e1a8e700045e04896317546ab459d649
hackerESQ <corey@coreyvarma.com>
2021-09-09 21:20:56 -0500

    fix protected registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
 
         <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('register') }}">
+        <form method="POST" action="{{ route('register', ['registration_key' => request('registration_key')]) }}">
             @csrf
 
             <div>
