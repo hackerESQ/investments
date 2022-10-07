ad005893614cc18fc490b7bf272d07fb1d9eb186
hackerESQ <corey@coreyvarma.com>
2022-10-07 17:21:20 -0500

    cleanup

--- resources/views/components/fifty-two-week-range.blade.php
+++ resources/views/components/fifty-two-week-range.blade.php
@@ -2,26 +2,15 @@
     
     @php
         $percent = (($current - $low) * 100) / ($high - $low);
-
-        $range = ['no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'];
-
     @endphp
 
-    @for ($x = 0; $x <= count($range); $x++)
+    @for ($x = 0; $x < 10; $x++)
         @php
-        if ($percent > ($x * 10)) {
-            $range[$x] = 'full';
-        }
-        @endphp
-    @endfor
-    
-    @foreach ($range as $range)
-        @php
-            if ($range=='full') {
+            if ($percent > ($x * 10)) {
                 echo " &#9679;";
             } else {
                 echo "	&#9675;";
             }
-        @endphp 
-    @endforeach
+        @endphp
+    @endfor
 </span>
