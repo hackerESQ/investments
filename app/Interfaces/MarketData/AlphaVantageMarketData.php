658bab94dec4aaf4d640e775c97965c2b9e861c8
hackerESQ <corey@coreyvarma.com>
2023-05-08 18:11:14 -0500

    testing new interfaces

---
+++ app/Interfaces/MarketData/AlphaVantageMarketData.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Interfaces\MarketData;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Collection;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
+
+class AlphaVantageMarketData implements MarketDataInterface
+{
+    // api client
+    public $client;
+    public $apiKey;
+
+    public function __construct() {
+
+        $this->client = Http::baseUrl('https://www.alphavantage.co/query?')->setMethod('GET');
+        $this->apiKey = 'demo';
+
+    }
+
+    public function exists(String $symbol): Bool
+    {
+        return $this->client->($symbol)->isNotEmpty();
+    }
+
+    public function quote($symbol): Collection
+    {
+        $quote = $this->client->getQuote($symbol);
+
+        if (empty($quote)) return collect();
+
+        return collect([
+            'name' => $quote->getLongName() ?? $quote->getShortName(),
+            'symbol' => $quote->getSymbol(),
+            'market_value' => $quote->getRegularMarketPrice(),
+            'fifty_two_week_high' => $quote->getFiftyTwoWeekHigh(),
+            'fifty_two_week_low' => $quote->getFiftyTwoWeekLow(),
+        ]);
+    }
+
+    public function dividends($symbol, $startDate, $endDate): Collection
+    {
+        return collect($this->client->getHistoricalDividendData($symbol, $startDate, $endDate))
+                        ->map(function($dividend) use ($symbol) {
+                            return [
+                                'symbol' => $symbol,
+                                'date' => $dividend->getDate()->format('Y-m-d H:i:s'),
+                                'dividend_amount' => $dividend->getDividends(),
+                            ];
+                        });
+    }
+
+    public function splits($symbol, $startDate, $endDate): Collection
+    {   
+        return collect($this->client->getHistoricalSplitData($symbol, $startDate, $endDate))
+                        ->map(function($split) use ($symbol) {
+                            $split_amount = explode(':', $split->getStockSplits());
+
+                            return [
+                                'symbol' => $symbol,
+                                'date' => $split->getDate()->format('Y-m-d H:i:s'),
+                                'split_amount' => $split_amount[0] / $split_amount[1],
+                            ];
+                        });
+    }
+}
